848b446da3b9c50dc8d87a7e0f006ab9412d3964
znzmektm2 <tingcber2@naver.com>
2020-04-24 23:47:54 +0900

    add: 라이프사이클 메서드 테스트

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import Contact from './Contact';
+import Test from './LifeCycle/Test';
 
 class App extends Component {
     render() {
         return (
             <div>
+                <Test/>
+                ==============================
                 <Contact /> 
             </div>
         );
